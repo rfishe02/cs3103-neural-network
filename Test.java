29e503df69b092910b6650ee74db801f69e85c7b
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-04-26 19:31:29 -0500

    Almost complete

--- Test.java
+++ Test.java
@@ -1,12 +1,17 @@
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Test {
 
   public static void main(String[] args) {
 
-    int input = 64;
+    ArrayList<Double> x = getInput("x1.txt",56);
+
+    int input = x.size()-1;
     int output = 3;
     int hidden = 8;
     int width = 4;
@@ -15,29 +20,59 @@ public class Test {
     n.buildNetwork(input,hidden,width,output);
     n.printWeights();
 
-    for(int i = 0; i < 50; i++) {
+    n.forwardPass(x);
+    n.backpropagate(1); // 1 for x, 2 for y, 3 for z
 
-      ArrayList<Double> data = new ArrayList<>(input+1);
+    System.out.println("\nAFTER\n");
+    n.printWeights();
 
-      for(int j = 0; j < input+1; j++) {
-        data.add(new Random().nextDouble() + 1);
-      }
-      n.forwardPass(data);
+  }
 
-      for(Double d : data) {
+  /** Read an 8 x 7 text file, with a letter written with the character X. */
 
-        if(d % 2 == 0) {
-          n.backpropagate(new Random().nextDouble() + 4);
-        } else {
-          n.backpropagate(new Random().nextDouble() + 2);
+  public static ArrayList<Double> getInput(String filename, int size) {
+
+    ArrayList<Double> output = new ArrayList<>(size+1);
+
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(filename));
+      String read;
+
+      while((read = br.readLine())!=null) {
+
+        for(int i = 0; i < read.length(); i++) {
+
+          if(Character.compare(read.charAt(i),'.') == 0) {
+            output.add(0.0);
+          } else {
+            output.add(1.0);
+          }
+
         }
 
       }
 
+    } catch(IOException ex) {
+      ex.printStackTrace();
     }
 
-    System.out.println("\nAFTER\n");
-    n.printWeights();
+    output.add(1.0);
+
+    return output;
+
+  }
+
+  /** Generate an input of a given size, with the added increment */
+
+  public static ArrayList<Double> generateInput(int input, int inc) {
+
+    ArrayList<Double> data = new ArrayList<>(input+1);
+
+    for(int j = 0; j < input+1; j++) {
+      data.add(new Random().nextDouble() + inc);
+    }
+
+    return data;
 
   }
 
